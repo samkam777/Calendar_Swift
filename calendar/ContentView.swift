d08fc7daf42bb14bf178776e1a437a352e3ee11f
samkam <samkam1010284222@gmail.com>
2024-01-17 20:25:10 +0800

    Title: swiftUI初始工程 Detail：as the title

---
+++ calendar/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  calendar
+//
+//  Created by samkam on 2024/1/8.
+//
+
+import SwiftUI
+
+func myselect() -> String{
+    return ["🌞✨", "🌛","🍎","🍐","📅"].randomElement()!
+}
+
+struct ContentView: View {
+    @State private var today = myselect()
+    var body: some View {
+        VStack{
+            Text("\(today)")
+                .font(.system(size: 100))
+                .padding()
+            Spacer()
+            Button("今天吃啥"){
+                today = myselect()
+            }
+            .padding()
+            .background(.teal)
+            .foregroundColor(.white)
+            .cornerRadius(40)
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+        }
+    }
+}
---
+++ calendar/calendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  calendarApp.swift
+//  calendar
+//
+//  Created by samkam on 2024/1/8.
+//
+
+import SwiftUI
+
+@main
+struct calendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
